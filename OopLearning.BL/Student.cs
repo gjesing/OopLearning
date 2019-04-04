0648ec31295190fc17fae0eeeb9b1aa05ad49220
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-04-04 09:59:08 +0200

    Added student class

---
+++ OopLearning.BL/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopLearning.BL
+{
+    public class Student : Person
+    {
+        private string unilogin;
+
+        public string UniLogin
+        {
+            get { return unilogin; }
+            set { unilogin = value; }
+        }
+
+    }
+}
