6345bfa48b5ae79ec76afe9abc05f1e159f11037
Maren Gjesing Laursen <mare0231@edu.campusvejle.dk>
2019-04-04 09:57:31 +0200

    Added employee class

---
+++ OopLearning.BL/Account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopLearning.BL
+{
+    class Account
+    {
+        private string accountNumber;
+        private string departmentNumber;
+        private decimal balance;
+
+        public string AccountNumber
+        {
+            get { return accountNumber; }
+            set { accountNumber = value; }
+        }
+        public string DepartmentNumber
+        {
+            get { return departmentNumber; }
+            set { departmentNumber = value; }
+        }
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+    }
+}
---
+++ OopLearning.BL/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OopLearning.BL
+{
+    public class Employee : Person
+    {
+        private string initials;
+
+        public string Initials
+        {
+            get { return initials; }
+            set { initials = value; }
+        }
+
+    }
+}
